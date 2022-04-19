bdc9f3ae527cca0c172c295c0af251c7487bf0ac
chylli-binary <chylli@binary.com>
2022-04-19 08:34:52 +0800

    Version 0.003

--- lib/Dist/Zilla/PluginBundle/Author/DERIV.pm
+++ lib/Dist/Zilla/PluginBundle/Author/DERIV.pm
@@ -4,7 +4,7 @@ package Dist::Zilla::PluginBundle::Author::DERIV;
 use strict;
 use warnings;
 
-our $VERSION = '0.002';
+our $VERSION = '0.003';
 our $AUTHORITY = 'cpan:DERIV'; # AUTHORITY
 
 =head1 NAME
